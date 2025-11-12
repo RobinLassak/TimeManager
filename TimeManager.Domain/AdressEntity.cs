c2ecccc2c7c429583bd9def5238026ab097b6c3a
Robin <lassak.robin@gmail.com>
2025-11-12 17:32:26 +0100

    Entita pro adresu

---
+++ TimeManager.Domain/AdressEntity.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimeManager.Domain
+{
+    public class AdressEntity : EntityBase
+    {
+        // Ulice
+        [Required, MaxLength(100)]
+        public string Street { get; set; } = null!;
+
+        // Cislo popisne
+        [Required, MaxLength(20)]
+        public string HouseNumber { get; set; } = null!;
+
+        // Cislo orientacni
+        [MaxLength(20)]
+        public string? OrientationNumber { get; set; }
+
+        // Mesto
+        [Required, MaxLength(100)]
+        public string City { get; set; } = null!;
+
+        // Kraj
+        [MaxLength(100)]
+        public string? Region { get; set; }
+
+        // PSC
+        [Required, MaxLength(20)]
+        public string ZipCode { get; set; } = null!;
+
+        // Stat
+        [Required, MaxLength(100)]
+        public string Country { get; set; } = null!;
+    }
+}
