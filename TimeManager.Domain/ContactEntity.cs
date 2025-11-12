38b98c95c0ba5e7f94c1ac2d26e4290398ac1bf9
Robin <lassak.robin@gmail.com>
2025-11-12 18:20:31 +0100

    entita pro kontakt

---
+++ TimeManager.Domain/ContactEntity.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimeManager.Domain
+{
+    // Typy kontaktu
+    public enum ContactType
+    {
+        Personal,
+        Business,
+        Other
+    }
+
+    // Zpusoby komunikace
+    public enum CommunicationType
+    {
+        Email,
+        Phone,
+    }
+    public class ContactEntity : EntityBase
+    {
+        // Zpusob komunikace
+        [Required]
+        public CommunicationType Communication { get; set; }
+
+
+        // Typ komunikace
+        [Required]
+        public ContactType Type { get; set; }
+
+        // Hodnota kontaktu (email, telefonni cislo, atd.)
+        [Required, MaxLength(100)]
+        public string Value { get; set; } = null!;
+
+        // Popis kontaktu
+        [MaxLength(500)]
+        public string? Description { get; set; }
+
+    }
+}
