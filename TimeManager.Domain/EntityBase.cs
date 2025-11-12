82423fb419f087b43fad0e96ae1744ca33192904
Robin <lassak.robin@gmail.com>
2025-11-12 17:13:47 +0100

    bazova entita

---
+++ TimeManager.Domain/EntityBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Domain
+{
+    // Bazova entita pro vsechny domenove tridy
+    public class EntityBase
+    {
+        public int Id { get; set; }
+    }
+}
