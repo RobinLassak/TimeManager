6dc2ed867e1505b87c13945d095faeaaf8494a6d
Robin <lassak.robin@gmail.com>
2025-11-13 07:36:58 +0100

    bazova trida pro kazdou osobu

---
+++ TimeManager.Domain/PersonBaseEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimeManager.Domain
+{
+    public abstract class PersonBaseEntity : EntityBase
+    {
+        // Jmeno
+        [Required, MaxLength(50)]
+        public string FirstName { get; set; } = null!;
+
+
+        // Prijmeni
+        [Required, MaxLength(50)]
+        public string LastName { get; set; } = null!;
+
+
+        // Datum narozeni
+        public DateTime? DateOfBirth { get; set; }
+
+        
+    }
+}
