9300eddb407f88708e10a7cd9edfd658b16cff61
Robin <lassak.robin@gmail.com>
2025-11-13 07:55:47 +0100

    Entita zakaznika

---
+++ TimeManager.Domain/CustomerEntity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeManager.Domain
+{
+    public class CustomerEntity : PersonBaseEntity
+    {
+        // Firma zakaznika
+        public CompanyEntity? Company { get; set; }
+
+
+        // Seznam kontaktu
+        public List<ContactEntity> Contacts { get; set; } = new();
+
+
+        // Seznam adres
+        public List<AdressEntity> Addresses { get; set; } = new();
+    }
+}
