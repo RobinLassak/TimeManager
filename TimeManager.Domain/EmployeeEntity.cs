5fdb98983fec025b502657811a34729135ad3309
Robin <lassak.robin@gmail.com>
2025-11-13 08:05:00 +0100

    Entita pro pracovnika

---
+++ TimeManager.Domain/EmployeeEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimeManager.Domain
+{
+    public class EmployeeEntity : PersonBaseEntity
+    {
+        // Uzivatelske jmeno
+        [MaxLength(100)]
+        public string? UserName { get; set; }
+
+
+        // Spolecnost
+        public CompanyEntity? Company { get; set; }
+
+
+        // Adresy
+        public List<AdressEntity> Addresses { get; set; } = new();
+
+
+        // Kontakty
+        public List<ContactEntity> Contacts { get; set; } = new();
+    }
+}
