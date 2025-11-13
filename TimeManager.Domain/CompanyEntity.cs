bf1ba52c4c01a127f125c252968197a8cef81d15
Robin <lassak.robin@gmail.com>
2025-11-13 07:52:14 +0100

    Entita firmy

---
+++ TimeManager.Domain/CompanyEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimeManager.Domain
+{
+    public class CompanyEntity : EntityBase
+    {
+        // Jmeno firmy
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+
+        // Ico firmy
+        [Required, MaxLength(20)]
+        public string Ico { get; set; } = null!;
+
+
+        // Dic firmy
+        [MaxLength(20)]
+        public string? Dic { get; set; }
+
+    }
+}
