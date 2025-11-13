a2add81a5ed6aced5137aeef19110884acec7b21
Robin <lassak.robin@gmail.com>
2025-11-13 08:37:27 +0100

    Entita pro Banku

---
+++ TimeManager.Domain/BankEntity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace TimeManager.Domain
+{
+    public class BankEntity : EntityBase
+    {
+        // Nazev banky
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+
+        // Kod banky
+        [Required, MaxLength(4)]
+        public string Code { get; set; } = null!;
+
+
+        // BIC/SWIFT kod
+        [MaxLength(11)]
+        public string? BicSwift { get; set; }
+    }
+}
